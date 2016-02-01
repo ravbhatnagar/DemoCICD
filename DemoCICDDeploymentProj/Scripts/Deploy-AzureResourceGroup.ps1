b0c2e5abb5f9db4d10257a7f7bd742462a11981a
Gaurav Bhatnagar <ravbhatnagar@hotmail.com>
2016-02-01 15:35:55 -0800

    Added Azure PS login to deploy script

--- DemoCICDDeploymentProj/Scripts/Deploy-AzureResourceGroup.ps1
+++ DemoCICDDeploymentProj/Scripts/Deploy-AzureResourceGroup.ps1
@@ -1,9 +1,15 @@
 ﻿$resourceGroupName = 'ravCICDDemoRG'
 $storageName = 'ravcicddemostorage'
 
+$password = "P@ssW0rd1"
+$certPath = "C:\certificates\examplecert4.pfx"
+
+$securePassword = ConvertTo-SecureString -String $password -Force -AsPlainText
+$cert1 = Import-PfxCertificate -FilePath $certPath cert:\LocalMachine\My -Password $securePassword
+
 Add-Type -Path 'C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ResourceManager\AzureResourceManager\AzureRM.Resources\Microsoft.Azure.Commands.Resources.dll'
 
-#Login-AzureRmAccount -TenantId 72f988bf-86f1-41af-91ab-2d7cd011db47 -ServicePrincipal -CertificateThumbprint $cert1.Thumbprint -ApplicationId 4d1165d0-08a3-444d-9863-8a0b9b75ffd6
+Login-AzureRmAccount -TenantId 72f988bf-86f1-41af-91ab-2d7cd011db47 -ServicePrincipal -CertificateThumbprint $cert1.Thumbprint -ApplicationId 4d1165d0-08a3-444d-9863-8a0b9b75ffd6
 
 Set-AzureStorageBlobContent -Context (Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Name $storageName).Context -Container packages -File 'C:\Program Files (x86)\Jenkins\jobs\Demo_CICD\workspace\DemoCICDDeploymentProj\bin\Debug\staging\DemoCICDWebApp\package.zip' -Force
 
