6adfd4566033ad92c20ed2b43e0d5859f849707d
Gaurav Bhatnagar <ravbhatnagar@hotmail.com>
2016-02-02 11:59:20 -0800

    Modifying sql deploy script

--- DemoCICDDeploymentProj/Tools/DeployDacPac.ps1
+++ DemoCICDDeploymentProj/Tools/DeployDacPac.ps1
@@ -1,8 +1,4 @@
-﻿Param(
-    [string] $dacpacFile,
-    [string] $batchFile
-)
-
+﻿
 Import-Module Azure -ErrorAction SilentlyContinue
 
 Set-StrictMode -Version 3
@@ -15,11 +11,15 @@ $cert1 = Import-PfxCertificate -FilePath $certPath cert:\LocalMachine\My -Passwo
 
 Login-AzureRmAccount -TenantId 72f988bf-86f1-41af-91ab-2d7cd011db47 -ServicePrincipal -CertificateThumbprint $cert1.Thumbprint -ApplicationId 4d1165d0-08a3-444d-9863-8a0b9b75ffd6
 
+$dacpacFile = "C:\Program Files (x86)\Jenkins\jobs\Demo_CICD\workspace\DemoCICDSqlProj\bin\Debug\DemoCICDSqlProj.dacpac"
 
 $connectionStringSecretValue = Get-AzureKeyVaultSecret -VaultName "ravCICDDemoKeyVault" -Name "sqlConnectionString"
 
 $connectionString = $connectionStringSecretValue.SecretValueText
 
-$commandScript =  "`"" + $batchFile + "`" " + "`"" + $dacpacFile + "`" " + "`"" + $connectionString + "`""
+#$commandScript =  "`"" + $batchFile + "`" " + "`"" + $dacpacFile + "`" " + "`"" + $connectionString + "`""
 
-cmd /c "`"$commandScript`""
+$command =  "`"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\SqlPackage.exe`"" + " /action:Publish " + "/SourceFile:"+ "`"" + $dacpacFile  + "`"" + " /tcs:" + $connectionString
+
+#cmd /c "`"$commandScript`""
+cmd $command
